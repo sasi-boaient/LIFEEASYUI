afbd547918bf2aa0673cb916a4beb6c59e680550
sasi-boaient <sasidharan.s@boaient.com>
2025-10-14 16:19:47 +0530

    build fix - turbo pack

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    // Use Webpack instead of Turbopack for build
     experimental: {
-        turbo: true,
+        turbo: false,
     },
 };
 
